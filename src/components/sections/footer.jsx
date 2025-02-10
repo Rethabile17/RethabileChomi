6924a64109c7de48df2c5dd5f030414cc6dcf689
peacendebele <peacendebele073@gmail.com>
2025-02-10 16:01:28 +0200

    did same update on css

--- src/components/sections/footer.jsx
+++ src/components/sections/footer.jsx
@@ -18,8 +18,8 @@ export const Footer = () => {
           <p className="flex items-center gap-2">
             <FontAwesomeIcon icon={faEnvelope} /> rethabilechomi610@gmail.com
           </p>
-          <p className="flex items-center gap-2">
-            <FontAwesomeIcon icon={faPhone} /> +27 68210 4225
+          <p className="flex items-center gap-2 ">
+            <FontAwesomeIcon icon={faPhone} /> <span className="text-blue-400">+27</span> 68210 4225
           </p>
        
         </div>
@@ -36,7 +36,7 @@ export const Footer = () => {
      
       <div className="text-center mt-6 border-t border-gray-700 pt-4">
         <p className="flex items-center justify-center gap-2 text-gray-400">
-          <FontAwesomeIcon icon={faCopyright} /> <span>2025</span> All Rights Reserved
+          <FontAwesomeIcon icon={faCopyright} /> <span className="text-blue-400 ">2025</span> All Rights Reserved
         </p>
       </div>
     </footer>
